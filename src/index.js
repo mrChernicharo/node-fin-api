197241e0d59a5437d0387ac34d31e024737182b9
Felipe Chernicharo <string7dev@gmail.com>
2021-03-10 00:02:25 -0300

    roteamento inicial get/post

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 const express = require("express");
+const { v4: uuid } = require("uuid");
 
 const app = express();
 app.use(express.json());
 
+customers = [];
+
 app.get("/", (request, response) => {
   return response.json({ message: "ok!" });
+});
+
+app.get("/accounts", (request, response) => {
+  return response.json(customers);
+});
+
+app.post("/accounts", (request, response) => {
+  const { name, cpf } = request.body;
+
+  if (customers.some((customer) => customer.cpf === cpf)) {
+    return response.status(400).json({ error: "CPF already in use" });
+  }
+
+  const customer = {
+    id: uuid(),
+    name,
+    cpf,
+    statement: [],
+  };
+  customers.push(customer);
+
+  return response.status(200).json(customer);
 });
 
 app.listen(3333, () => {
